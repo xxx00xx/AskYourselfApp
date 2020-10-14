0f1b435a496b8942f93f549c592bed8e35095fd1
Shotaro Furuta <zoxx31syg1y999@gmail.com>
2020-10-14 16:03:51 +0900

    更新

--- ChatApp/QuestionerViewController.swift
+++ ChatApp/QuestionerViewController.swift
@@ -15,11 +15,18 @@ class QuestionerViewController: UIViewController {
     var chat: Chat?
     let realm = try! Realm()
     
+    private let cellId = "cellId"
+    @IBOutlet weak var chatQuestionerRoomTableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // タイトル文字列の設定
         self.navigationItem.title = text
+        //chatQuestionerRoomTableView.backgroundColor = .green
+        chatQuestionerRoomTableView.delegate = self
+        chatQuestionerRoomTableView.dataSource = self
+        chatQuestionerRoomTableView.regi
         /*
         //トップに戻るボタンを作成
         let leftButton = UIBarButtonItem(
@@ -29,6 +36,19 @@ class QuestionerViewController: UIViewController {
         action: nil)
         self.navigationItem.backBarButtonItem = leftButton
          */
+        
+        
     }
 }
 
+extension QuestionerViewController: UITableViewDelegate, UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = chatQuestionerRoomTableView.dequeueReusableCell(withIdentifier: cellId, for: indexPath)
+        cell.backgroundColor = .purple
+        return cell
+    }
+}
